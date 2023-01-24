59f5e10a82de2e492bb6029b3da612e59ccf9fab
jbiko <biteetejeremyb@stanbic.com>
2023-01-24 15:47:28 +0300

    Add better structure to application as well as routing and central store

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container, Button } from '@mui/material';
 import { PhotoCamera } from '@mui/icons-material';
 
+import { Route, Routes } from 'react-router-dom';
+import PhotoAlbum from './pages/photoAlbum';
 
 
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 function App() {
   return (
     <>
@@ -20,56 +21,9 @@ function App() {
         </Toolbar>
       </AppBar>
       <main>
-        <div>
-          <Container >
-            <Typography variant='h2' align='center' gutterBottom>
-              Photo album
-            </Typography>
-            <Typography variant='paragraph' align='center' color='GrayText'>
-              What is Lorem Ipsum?
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-            </Typography>
-
-            <div>
-              <Grid container spacing={2} justifySelf >
-                <Grid item>
-                  <Button variant='contained' color='primary'>
-                    see my photos
-                  </Button>
-                </Grid>
-                <Grid item>
-                  <Button variant='outlined' color='error'>
-                    go back
-                  </Button>
-                </Grid>
-              </Grid>
-            </div>
-            <Grid container sx={{ paddingTop: 2  }} spacing={4} >
-              {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4} >
-                <Card >
-                  <CardMedia sx={{ height: 250, width: '100%' }} image='https://res.cloudinary.com/two-three-apps/image/upload/v1664790809/handy1_wge62s.jpg' ></CardMedia>
-                  <CardContent>
-                    <Typography variant='h5' gutterBottom >
-                      Heading
-                    </Typography>
-                    <Typography gutterBottom >
-                      What is Lorem Ipsum?
-                      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button color='primary'>
-                      see my photos
-                    </Button>
-                  </CardActions>
-                </Card>
-              </Grid>                
-              ))}
-            </Grid>
-
-          </Container>
-        </div>
+        <Routes>
+          <Route path='/' element={<PhotoAlbum />} />
+        </Routes>
       </main>
     </>
   );
---
+++ src/pages/photoAlbum.js
@@ -0,0 +1,35 @@
+import { Card, CardActions, CardContent, CardMedia, Container, Grid, Typography, Button } from "@mui/material"
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+function PhotoAlbum() {
+    return (
+        <div>
+            <Container>
+                <Grid container sx={{ paddingTop: 2  }} spacing={4} >
+                {cards.map((card) => (
+                <Grid item key={card} xs={12} sm={6} md={4} >
+                    <Card >
+                    <CardMedia sx={{ height: 250, width: '100%' }} image='https://res.cloudinary.com/two-three-apps/image/upload/v1664790809/handy1_wge62s.jpg' ></CardMedia>
+                    <CardContent>
+                        <Typography variant='h5' gutterBottom >
+                        Heading
+                        </Typography>
+                        <Typography gutterBottom >
+                        What is Lorem Ipsum?
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button color='primary'>
+                        see my photos
+                        </Button>
+                    </CardActions>
+                    </Card>
+                </Grid>                
+                ))}
+                </Grid>
+            </Container>            
+        </div>
+    )
+}
+export default PhotoAlbum;
