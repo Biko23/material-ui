970747914e52b2851a303d29685727cc3041cc0e
jbiko <biteetejeremyb@stanbic.com>
2023-01-24 13:33:23 +0300

    Make image width fill width of card

--- src/App.js
+++ src/App.js
@@ -45,9 +45,9 @@ function App() {
             </div>
             <Grid container sx={{ paddingTop: 2  }} spacing={4} >
               {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={3} >
+              <Grid item key={card} xs={12} sm={6} md={4} >
                 <Card >
-                  <CardMedia sx={{ height: 250, width: 250 }} image='https://res.cloudinary.com/two-three-apps/image/upload/v1664790809/handy1_wge62s.jpg' ></CardMedia>
+                  <CardMedia sx={{ height: 250, width: '100%' }} image='https://res.cloudinary.com/two-three-apps/image/upload/v1664790809/handy1_wge62s.jpg' ></CardMedia>
                   <CardContent>
                     <Typography variant='h5' gutterBottom >
                       Heading
